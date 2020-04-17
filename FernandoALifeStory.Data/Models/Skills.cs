2a6ee24137224fa72ab17698388dc4170082a816
SirNanwar <fernando.guerra.duarte@gmail.com>
2020-04-17 08:59:07 +0100

    0.9 - Made changes to Course data, working on skill representation

--- FernandoALifeStory.Data/Models/Skills.cs
+++ FernandoALifeStory.Data/Models/Skills.cs
@@ -1,12 +1,19 @@
-﻿namespace FernandoALifeStory.Data.Models
+﻿using System.ComponentModel;
+
+namespace FernandoALifeStory.Data.Models
 {
     public enum Skills
     {
-        UNDEFINED,
-        CSharp,
-        dotNET,
-        ASPdotNET,
-        MVC,
-        VisualStudio
+        UNDEFINED = 0,
+        [Description("C#")]
+        CSharp = 1,
+        [Description(".NET")]
+        dotNET = 2,
+        [Description("ASP.NET")]
+        ASPdotNET = 3,
+        [Description("MVC")]
+        MVC = 4,
+        [Description("Microsoft Visual Studio")]
+        VisualStudio = 5
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Fernando A Life Story
-## Version 0.8
+## Version 0.9
 
 Personal portfolio for my academic and professional achievements. 
 
